36ac77f4ad716e77f42367c4eab0c2e60c083dad
Star Dorminey <sterlind@microsoft.com>
2019-11-19 19:10:26 -0800

    Broke something in iterator somehow.

--- LibGit2Sharp.Tests/ConfigurationBackendFixture.cs
+++ LibGit2Sharp.Tests/ConfigurationBackendFixture.cs
@@ -1,5 +1,6 @@
 ﻿using LibGit2Sharp.Tests.TestHelpers;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,11 +16,89 @@ namespace LibGit2Sharp.Tests
         {
             var backend = new MockConfigBackend();
             backend.Entries["foo.bar"] = "baz";
-            using (var repo = new Repository(SandboxBareTestRepo()))
+            using (var repo = Init(backend))
             {
-                repo.Config.AddBackend(backend, ConfigurationLevel.System, true);
                 Assert.Equal("baz", repo.Config.Get<string>("foo.bar").Value);
             }
+        }
+
+        [Fact]
+        public void Set()
+        {
+            var backend = new MockConfigBackend();
+            using (var repo = Init(backend))
+            {
+                SetValue(repo, "foo.bar", "quux");
+                Assert.Equal("quux", GetValue(repo, "foo.bar"));
+            }
+        }
+
+        [Fact]
+        public void Delete()
+        {
+            var backend = new MockConfigBackend();
+            using (var repo = Init(backend))
+            {
+                SetValue(repo, "foo.bar", "quux");
+                Assert.Equal("quux", GetValue(repo, "foo.bar"));
+                DeleteValue(repo, "foo.bar");
+                Assert.Null(GetValue(repo, "foo.bar"));
+            }
+        }
+
+        [Fact]
+        public void Iterate()
+        {
+            var backend = new MockConfigBackend();
+            backend.Entries["foo.bar"] = "baz";
+            backend.Entries["foo.baz"] = "quux";
+            using (var repo = Init(backend))
+            {
+                repo.Config.AddBackend(backend, ConfigurationLevel.System, true);
+                AssertConfigsMatch(backend.Entries, repo.Config);
+            }
+        }
+
+        private Repository Init(MockConfigBackend backend)
+        {
+            var repo = new Repository(InitNewRepository(true));
+            repo.Config.AddBackend(backend, ConfigurationLevel.System, true);
+            return repo;
+        }
+
+        private string GetValue(Repository repo, string key)
+        {
+            var entry = repo.Config.Get<string>(key, ConfigurationLevel.System);
+            if (entry == null)
+            {
+                return null;
+            }
+
+            Assert.Equal(ConfigurationLevel.System, entry.Level);
+            return entry.Value;
+        }
+
+        private void SetValue(Repository repo, string key, string value)
+        {
+            repo.Config.Set(key, value, ConfigurationLevel.System);
+        }
+
+        private void DeleteValue(Repository repo, string key)
+        {
+            Assert.True(repo.Config.Unset(key, ConfigurationLevel.System));
+        }
+
+        private static void AssertConfigsMatch(IReadOnlyDictionary<string, string> expected, IEnumerable<ConfigurationEntry<string>> actual)
+        {
+            var sortedActual = actual
+                .Where(entry => expected.ContainsKey(entry.Key))
+                .Select(entry => new Tuple<string, string>(entry.Key, entry.Value))
+                .ToArray();
+
+            Array.Sort(sortedActual);
+            var sortedExpected = expected.Select(entry => new Tuple<string, string>(entry.Key, entry.Value)).ToArray();
+            Array.Sort(sortedExpected);
+            Assert.Equal(sortedExpected, sortedActual);
         }
 
         private class MockReadOnlyConfigBackend : ReadOnlyConfigBackend
@@ -49,11 +128,14 @@ namespace LibGit2Sharp.Tests
 
         private class MockConfigBackend : ReadWriteConfigBackend
         {
-            public readonly Dictionary<string, string> Entries = new Dictionary<string, string>();
+            public readonly SortedDictionary<string, string> Entries = new SortedDictionary<string, string>();
 
             public override void Del(string key)
             {
-                throw new NotImplementedException();
+                if (!Entries.Remove(key))
+                {
+                    throw ConfigBackendException.NotFound(key);
+                }
             }
 
             public override void DelMultiVar(string name, string regexp)
@@ -73,7 +155,7 @@ namespace LibGit2Sharp.Tests
 
             public override IEnumerable<string> Iterate()
             {
-                throw new NotImplementedException();
+                return Entries.Keys;
             }
 
             public override void Lock()
@@ -83,7 +165,7 @@ namespace LibGit2Sharp.Tests
 
             public override void Set(string key, string value)
             {
-                throw new NotImplementedException();
+                Entries[key] = value;
             }
 
             public override void SetMultiVar(string name, string regexp, string value)
